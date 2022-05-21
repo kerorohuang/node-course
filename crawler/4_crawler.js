c7f13891441cd3e1a1e9092446d4002c6558ac7b
Hannah <4428keroro@gmail.com>
2022-05-21 14:29:56 +0800

    use dotenv to hide the inportant details

--- crawler/4_crawler.js
+++ crawler/4_crawler.js
@@ -5,14 +5,15 @@
 // 引用進來
 
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 (async () => {
   const connection = await mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "admin",
-    password: "",
-    database: "node_js",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   let [data, fields] = await connection.execute("SELECT * FROM stocks");
