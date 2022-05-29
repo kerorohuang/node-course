6514292762b5c180d81627cbec77d73d9c84feee
Hannah <4428keroro@gmail.com>
2022-05-29 16:34:49 +0800

    import back end data to render on website

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,17 +1,98 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const StockDetails = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+
+  const { stockId } = useParams();
+
+  useEffect(() => {
+    let getPrice = async () => {
+      let response = await axios.get(`http://localhost:3001/stocks/${stockId}`, {
+        params: {
+          page: page,
+        },
+      });
+      setData(response.data.data);
+      setTotalPage(response.data.pagination.totalPage);
+    };
+    getPrice();
+  }, [page]);
+
+  const getPages = () => {
+    let pages = [];
+    for (let i = 1; i <= totalPage; i++) {
+      // page 是我們現在在第幾頁
+      pages.push(
+        <li
+          style={{
+            display: "inline-block",
+            margin: "2px",
+            backgroundColor: page === i ? "#00d1b2" : "",
+            borderColor: page === i ? "#00d1b2" : "#dbdbdb",
+            color: page === i ? "#fff" : "#363636",
+            borderWidth: "1px",
+            width: "28px",
+            height: "28px",
+            borderRadius: "3px",
+            textAlign: "center",
+          }}
+          key={i}
+          onClick={(e) => {
+            setPage(i);
+          }}
+        >
+          {i}
+        </li>
+      );
+    }
+    return pages;
+    // pages.push(1); // [1]
+    // pages.push(2); // [1, 2]
+  };
+
   return (
     <div>
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：</h2>
-      </div>
+      <ul>{getPages()}</ul>
+      {data.map((item) => {
+        return (
+          <div
+            key={item.date}
+            className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6"
+          >
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              日期：{item.date}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交金額：{item.amount}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交股數：{item.volume}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              開盤價：{item.open_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              收盤價：{item.close_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              漲跌價差：{item.delta_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最高價：{item.high_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最低價：{item.low_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交筆數：{item.transactions}
+            </h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
