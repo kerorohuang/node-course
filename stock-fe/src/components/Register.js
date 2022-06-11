e7854146cd342e1ff0394f9a817e238af5857b47
Hannah <4428keroro@gmail.com>
2022-06-11 16:33:06 +0800

    set state for the register surface

--- stock-fe/src/components/Register.js
+++ stock-fe/src/components/Register.js
@@ -1,24 +1,73 @@
+import { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../utils/config";
+
 const Register = () => {
+  const [member, setMember] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+    photo: "",
+  });
+
+  function handelChange(e) {
+    setMember({ ...member, [e.target.name]: e.target.value });
+  }
+
+  async function handelSubmit(e) {
+    e.preventDefault();
+    try {
+      let response = await axios.post(`${API_URL}/auth/register`, member);
+      console.log(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <form className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center">
-      <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">註冊帳戶</h2>
+      <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">
+        註冊帳戶
+      </h2>
       <div className="mb-4 text-2xl">
         <label htmlFor="name" className="flex mb-2 w-32">
           Email
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="email" name="email" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="email"
+          name="email"
+          value={member.email}
+          onChange={handelChange}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="name" className="flex mb-2 w-32">
           姓名
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="text" id="name" name="name" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="text"
+          id="name"
+          name="name"
+          value={member.name}
+          onChange={handelChange}
+        />
       </div>
       <div className="mb-4 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-16">
           密碼
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="password" id="password" name="password" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="password"
+          id="password"
+          name="password"
+          value={member.password}
+          onChange={handelChange}
+        />
       </div>
       <div className="mb-8 text-2xl">
         <label htmlFor="password" className="flex mb-2 w-32">
@@ -29,15 +78,26 @@ const Register = () => {
           type="password"
           id="confirmPassword"
           name="confirmPassword"
+          value={member.confirmPassword}
+          onChange={handelChange}
         />
       </div>
       <div className="mb-8 text-2xl">
         <label htmlFor="photo" className="flex mb-2 w-32">
           圖片
         </label>
-        <input className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2" type="file" id="photo" name="photo" />
+        <input
+          className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
+          type="file"
+          id="photo"
+          name="photo"
+          value={member.photo}
+          onChange={handelChange}
+        />
       </div>
-      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in">註冊</button>
+      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in" onClick={handelSubmit}>
+        註冊
+      </button>
     </form>
   );
 };
